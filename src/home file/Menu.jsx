1fb03e9c8c1ff70431ffa45ca3b0a3ed8d898124
minhazapon <minhajulabedinapon@gmail.com>
2024-10-29 03:55:01 -0700

    added banner files

--- src/home file/Menu.jsx
+++ src/home file/Menu.jsx
@@ -1,10 +1,11 @@
 
+import '../css file/Universe.css'
 
 const Menu = () => {
 
 
     return (
-        <div className=" mb-12 flex justify-center p-5 " 
+        <div className=" mb-12 flex justify-center p-10 " 
         
         style={{
             backgroundImage: "url(https://img.freepik.com/free-vector/textured-background-design_1117-109.jpg?t=st=1730195657~exp=1730199257~hmac=28d38b33ad92a65a985c145b29b1c8c16ce14c7ad9b2ba85f06ac34402ed8bf9&w=740)",
@@ -13,12 +14,15 @@ const Menu = () => {
         >
 
 
-            <div className=" text-white grid md:grid-cols-2 gap-10 ">
+            <div className=" text-white grid md:grid-cols-2 gap-24 ">
 
                
                 <div>
-                   <p className=" font-serif text-xl ">TASTY & FRESH</p>
-                   <p className=" font-serif text-4xl font-bold mt-2 ">HOT DOGS</p>
+                   <p className=" text-yellow-300 font-serif text-xl ">TASTY & FRESH</p>
+                   <div className=' flex items-center gap-2'>
+                   <img className=' h-[30px] ' src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png" alt="" />
+                   <p className=" the font-serif text-4xl font-bold mt-2 ">HOT DOGS</p>
+                   </div>
                    <div>
                    <div className=" flex justify-between  gap-10 mt-5">
                      <p className=" font-serif text-2xl  ">WASTED HOTDOG<br></br>
@@ -54,12 +58,15 @@ const Menu = () => {
 
 
                 <div>
-                   <p className=" font-serif text-xl ">TASTY & FRESH</p>
-                   <p className=" font-serif text-4xl font-bold mt-2 ">HOT DOGS</p>
+                   <p className=" text-yellow-300 font-serif text-xl ">TASTY & FRESH</p>
+                   <p className=" the font-serif text-4xl font-bold mt-2 ">SANDWICHES</p>
                    <div>
                    <div className=" flex  justify-between  gap-10 mt-5">
-                     <p className=" font-serif text-2xl  ">WASTED HOTDOG<br></br>
-                      <span className=" text-xl text-gray-400 ">Heirloom cherry tomatoes, high-quality extra-virgin olive oil</span></p>
+                    <div className=' flex items-center gap-2 '>
+                    <p className=" font-serif text-2xl  ">WASTED HOTDOG<br></br>
+                    <span className=" text-xl text-gray-400 ">Heirloom cherry tomatoes, high-quality extra-virgin olive oil</span></p>
+                    </div>
+                    
                      <p className=" text-xl ">$4.00</p> 
                    </div>
                    <div className=" mt-5 ">
@@ -90,8 +97,8 @@ const Menu = () => {
  
 
                 <div>
-                   <p className=" font-serif text-xl ">TASTY & FRESH</p>
-                   <p className=" font-serif text-4xl font-bold mt-2 ">HOT DOGS</p>
+                   <p className=" text-yellow-300 font-serif text-xl ">TASTY & FRESH</p>
+                   <p className=" the font-serif text-4xl font-bold mt-2 ">WAFFLES</p>
                    <div>
                    <div className=" flex  justify-between  gap-10 mt-5">
                      <p className=" font-serif text-2xl  ">WASTED HOTDOG<br></br>
@@ -127,8 +134,11 @@ const Menu = () => {
 
 
                 <div>
-                   <p className=" font-serif text-xl ">TASTY & FRESH</p>
-                   <p className=" font-serif text-4xl font-bold mt-2 ">HOT DOGS</p>
+                   <p className=" text-yellow-300 font-serif text-xl ">TASTY & FRESH</p>
+                   <div className=' flex items-center gap-2'>
+                   <img className=' h-[30px] ' src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png" alt="" />
+                   <p className=" the font-serif text-4xl font-bold mt-2 ">SIDES & DRINKS</p>
+                   </div>
                    <div>
                    <div className=" flex justify-between   gap-10 mt-5">
                      <p className=" font-serif text-2xl  ">WASTED HOTDOG<br></br>
